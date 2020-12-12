68af897468a89d8d46da5030c3046dff39e34605
Madhav Milan <49267441+m2-madhav@users.noreply.github.com>
2020-12-12 18:12:48 +0530

    code-added

--- src/index.js
+++ src/index.js
@@ -1,19 +1,29 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 const bodyParser = require("body-parser");
-const port = 8080
+const port = 8080;
 app.use(express.urlencoded());
 
 // Parse JSON bodies (as sent by API clients)
 app.use(express.json());
-const { data } = require('./data')
+const { data } = require("./data");
 
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 // your code goes here
 
+app.get("/topRankings", (req, res) => {
+  const offset = parseInt(req.query.offset);
+  const limit = parseInt(req.query.limit);
 
-app.listen(port, () => console.log(`App listening on port ${port}!`))
+  const startIndex = offset;
+  const endIndex = limit;
+
+  const result = data.slice(startIndex, endIndex);
+  res.json(result);
+});
+
+app.listen(port, () => console.log(`App listening on port ${port}!`));
 
 module.exports = app;
